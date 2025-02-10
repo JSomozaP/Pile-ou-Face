e374a472a442dd352a99490ff189e8ba893155aa
JSomozaP <jeremy.somoza@gmail.com>
2025-02-10 11:31:11 +0100

    suite de bidouillage multiple, et ça ne fonctionne toujours pas comme il faut

--- Pile ou Face.c
+++ Pile ou Face.c
@@ -12,53 +12,72 @@ int reportho(char * reponse){
     return PILE;
     if (strcasecmp(reponse, "2") == 0 || (strcasecmp(reponse, "face") == 0))
     return FACE;
-    if (strcasecmp(reponse, "0") == 0 || (strcasecmp(reponse, "exit") == 0))
+    if (strcasecmp(reponse, "0") == 0 || (strcasecmp(reponse, "quitter") == 0))
     return 0;
 return -1;
+
+//strcasecmp permet de comparer des chaînes de caractère tout en étant insensible à la casse, contrairement à strcmp
     
 }
  
 int main(){
 	
+    srand(time(NULL));
     int reussites = 0, echecs = 0;
     char reponse [5];
     int choix, resultat;
-    srand(time(NULL));
-    resultat=(rand()%2) + 1;
 
     
-    while(1) {
-    printf("###### Pile ou Face ######\n\n\n");
-    printf("Pile ou Face ?\n");
-    printf("1. Pile\n");
-    printf("2. Face\n");
-    printf("0. Quitter\n");
-    fgets(reponse,5,stdin);
+    while (1) {
+        printf("###### Pile ou Face ######\n\n\n");
+        printf("Pile ou Face ?\n");
+        printf("1. Pile\n");
+        printf("2. Face\n");
+        printf("0. Quitter\n");
+        
+        fgets(reponse,sizeof(reponse),stdin);
+        reponse[strcspn(reponse, "\n")]=0; //strcspn permet de comparer l'occurence de caractère d'une string à l'autre
+        choix = reportho(reponse);
 
-    if (choix == 0) {
-        printf ("Vous quittez le jeu\n");
+        if (choix == 0) {
+            printf ("Vous quittez le jeu\n");
+            break;
+        }
+        else if (choix == -1) {
+            printf ("Choix invalide\n");
+            continue;
+        } 
+
     }
-    else if (choix == -1) {
-        printf ("Choix invalide\n");
-    } 
+printf("\nvous faites %s \n", choix == PILE ? "Pile":"Face");
+printf("\nLa pièce est jetée\n");
+for (int i=0; i < 3; i++){
+    sleep(1);
 
-    }
-
-
+    resultat=(rand()%2) + 1;  // 1 = PILE et 2 = FACE
+    printf("%s \n\n", resultat == PILE ? "PILE" :"FACE");
+}
     if (choix == resultat) {
-        if (resultat == 0){
-            printf("\nPile !\n\nC'est gagné, bien joué !\n");
+        printf("\nC'est gagné, bien joué !\n");
+        reussites++;
         }
         else {
-            printf("\nFace !\n\nC'est gagné, bien joué !\n");
-        }
-    } else {
-        if (resultat == 0){
-            printf("\nPile !\n\nC'est perdu, dommage ...\n");
-        }
-         else {
-            printf("\nFace !\n\nC'est perdu, dommage ...\n");
-        }
+        printf("\nC'est perdu, dommage !\n");
+        echecs++;
+    
     }
+
+    printf("reussites : %d\nEchecs : %d\n\n",reussites, echecs);
+    printf("|--------------------------|\n");
+
+    if (echecs >= 10) {
+        printf("Trop d'echecs\n");
+    }
+
+    printf("##########GAME OVER##########\n");
+    printf("reussites : %d\nEchecs : %d\n", reussites, echecs);
+    printf("|-------------------------|\n");
+
     return 0;
     }
+    
