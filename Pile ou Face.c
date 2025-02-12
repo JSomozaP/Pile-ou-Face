6c4e82412c648fa1e2b350bd88cea9f8672d3d9b
JSomozaP <jeremy.somoza@gmail.com>
2025-02-12 15:05:58 +0100

    checkpoint 1

--- Pile ou Face.c
+++ Pile ou Face.c
@@ -6,27 +6,43 @@
 
 #define PILE 1
 #define FACE 2
+#define QUITTER 0
+
 
 void convertirmin(char *str) {
     for (int i=0; str[i] != '\0'; i++) {
         if (str[i]>='A' && str[i] <= 'Z') {
-            str[i] = str[i] + ('a'-'A');
+            printf("%c\t",str[i]);
+            // str[i] = 'e';
+            // str[i] = str[i] + ('a'-'A');
             str[i] = str[i] +32; //si les caractères sont en majuscules, ajouter 32 à leur valeur ASCII les convertiront en minuscule
+        }else if(str[i]<='a' && str[i]>='z'){
+            str[i]=str[i] -32;
+
         }
     }
 }
 
 int reportho(char * reponse){
-    reponse[strcspn(reponse, "\n")]='\0'; //strcspn permet de comparer l'occurence de caractère d'une string à l'autre
-  
-    if (strcmp(reponse, "1") == 0 || (strcmp(reponse, "pile") == 0))
-    return PILE;
-    if (strcmp(reponse, "2") == 0 || (strcmp(reponse, "face") == 0))
-    return FACE;
-    if (strcmp(reponse, "0") == 0 || (strcmp(reponse, "quitter") == 0))
-    return 0;
-return -1;
+    size_t longueur = strlen(reponse);
+    if (longueur > 0 && reponse [longueur - 1]== '\n') {
+            reponse[longueur -1] = '\0';
+    }
 
+    convertirmin(reponse);
+    
+    if (strcmp(reponse, "1") == 0 || (strcmp(reponse, "pile") == 0)|| (strcmp(reponse, "PILE") == 0) || (strcmp(reponse, "pIle") == 0) || (strcmp(reponse, "p") == 0))  //Répondre 1, pile, PILE, pILe ou même p permettent de jouer PILE
+        return PILE;
+    if (strcmp(reponse, "2") == 0 || (strcmp(reponse, "face") == 0)||(strcmp(reponse, "FACE") == 0) || (strcmp(reponse, "fAcE") == 0) || (strcmp(reponse, "f") == 0))  //Répondre 2, face, FACE, fAcE ou même f permettent de jouer FACE
+        return FACE;
+    if (strcmp(reponse, "0") == 0 || (strcmp(reponse, "quitter") == 0)||(strcmp(reponse, "quit") == 0) || (strcmp(reponse, "exit") == 0) || (strcmp(reponse, "q") == 0))   //Répondre 0, quitter, quit, exit ou même q permettent de jouer PILE
+        return 0;
+    
+    return -1;
+
+    /* interdit pour cet exo là :
+    reponse[strcspn(reponse, "\n")]='\0'; //strcspn permet de comparer l'occurence de caractère d'une string à l'autre */
+  
 //strcasecmp permet de comparer des chaînes de caractère tout en étant insensible à la casse, contrairement à strcmp
     /*if (strcasecmp(reponse, "1") == 0 || (strcasecmp(reponse, "pile") == 0))
     return PILE;
@@ -39,7 +55,13 @@ return -1;
 }
  
 int main(){
-	
+	// char prenom[] = "MassiNissa";
+    // convertirmin(prenom);
+    // printf("%s\n",prenom);
+
+    // return 0;
+
+
     srand(time(NULL));
     int reussites = 0, echecs = 0;
     char reponse [10];
@@ -51,9 +73,12 @@ int main(){
         printf("Pile ou Face ?\n");
         printf("1. Pile\n");
         printf("2. Face\n");
-        printf("0. Quitter\n");
+        printf("0. Quitter\n\n");
         
-        fgets(reponse,sizeof(reponse),stdin);
+        if (!fgets(reponse,sizeof(reponse),stdin)) {
+            printf("\nErreur de saisie, veuillez réessayer !\n\n");
+            continue;
+        }
     
         
         choix = reportho(reponse);
@@ -69,22 +94,22 @@ int main(){
         } 
 
     }
-printf("\nvous faites %s \n", (choix == PILE) ? "Pile":"Face");
-printf("\nLa pièce est jetée\n");
-for (int i=0; i < 3; i++){
-    sleep(1);
+    printf("\nvous faites %s \n", (choix == PILE) ? "Pile":"Face");
+    printf("\nLa pièce est jetée\n\n");
+    for (int i=0; i < 3; i++){
+        sleep(1);
 
-    resultat=(rand()%2) + 1;  // 1 = PILE et 2 = FACE
-    printf("%s \n\n", (resultat == PILE) ? "PILE" :"FACE");
-}
-            if (choix == resultat) {
-            printf("\nC'est gagné, bien joué !\n");
-            reussites++;
-            }
-            else {
-            printf("\nC'est perdu, dommage !\n");
-            echecs++;
-        }
+        resultat=(rand()%2) + 1;  // 1 = PILE et 2 = FACE
+        printf("%s \n\n", (resultat == PILE) ? "PILE" :"FACE");
+    }
+    if (choix == resultat) {
+        printf("\nC'est gagné, bien joué !\n");
+        reussites++;
+    }
+    else {
+        printf("\nC'est perdu, dommage !\n");
+        echecs++;
+    }
 
     printf("reussites : %d\nEchecs : %d\n\n",reussites, echecs);
     printf("|--------------------------|\n");
